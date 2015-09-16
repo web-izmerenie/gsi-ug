6734bc3c054963c39786b4ff321605cc8cfb6fc0
gsi-ug <gsi-ug@Debian-60-squeeze-64-minimal.(none)>
2015-09-16 16:31:10 +0300

    Init bitrix

---
+++ bitrix/templates/main/footer.php
@@ -0,0 +1,28 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+?>
+</main>
+<footer>
+	<div class="wrapper">
+		<div class="copy">© ГидроСтройИзоляция, 2015-2016 </div>
+		<div class="develop"><a href="http://web-izmerenie.ru">Сделано в</a></div>
+	</div>
+</footer>
+<div class="overlay"></div>
+<div id="form" class="animated">
+	<form>
+		<label>Ваше имя:</label>
+		<input type="text" name="name" maxlength="45" required>
+		<label>Телефон или e-mail:</label>
+		<input type="text" name="tel" maxlength="45" required>
+		<label>Опишите вашу задачу:</label>
+		<textarea type="text" name="message" maxlength="300" required></textarea>
+		<div class="error">Сообщение не отправлено. Пожалуйста, заполните все поля».</div><a href="#submit" class="btn"><span>Отправить заявку</span></a>
+	</form>
+	<div class="sucsess">Спасибо за ваше обращение.<br>
+		Мы свяжемся с вами в ближайшее время.
+	</div>
+</div>
+</body>
+</html>
---
+++ bitrix/templates/main/header.php
@@ -0,0 +1,34 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$tplPath = $APPLICATION->GetTemplatePath();
+global $tplPath;
+?>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content-width="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <?$APPLICATION->ShowHead();?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?=$tplPath;?>/styles/build/build.css">
+    <script type="text/javascript" src="<?=$tplPath;?>/scripts/build/build.js"></script>
+  </head>
+  <body>
+		<?$APPLICATION->ShowPanel();?>
+    <header>
+      <div class="wrapper"><img src="<?=$tplPath;?>/img/logo.png">
+        <nav id="menu">
+          <ul>
+            <li><a href="#application">Услуги</a></li>
+            <li><a href="#steps">Технологии</a></li>
+            <li><a href="#sales">Акция</a></li>
+            <li><a href="#contacts">Контакты</a></li>
+          </ul>
+        </nav><a href="#form" class="btn"><span>Отправить заявку</span></a>
+      </div>
+    </header>
+    <main class="content">
