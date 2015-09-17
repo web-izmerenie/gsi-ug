bdb319d380b6617f088c4427bfacafdc37c8d249
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-09-17 13:37:29 +0300

    Add include area from block your benefits

--- index.php
+++ index.php
@@ -170,45 +170,19 @@ $APPLICATION->SetTitle('Главная');
 	),
 	false
 );?>
-<section id="your-benefits">
-	<div class="wrapper">
-		<h1>Преимущества для Вас</h1>
-		<ul>
-			<li>
-				<p>Мы предлагаем вам<br>гибкий график платежей</p>
-				<div class="text">
-					Платежи за работу и материалы вносятся частями по графику платежей
-					после выполнения определенных этапов работ.
-				</div>
-			</li>
-			<li>
-				<p>Гарантия нашей<br>работы – 7 лет</p>
-				<div class="text">
-					Мы выдаем гарантийные документы и выполняем ремонтные работы
-					технологических повреждений.
-				</div>
-			</li>
-			<li>
-				<p>Мы строго соблюдаем сроки выполнения работ</p>
-				<div class="text">
-					Вы можете быть уверены
-					в том, что работа будет выполнена в срок. График выполнения
-					работ подтвержден договором.
-				</div>
-			</li>
-			<li>
-				<p>Мы предоставляем комплексное обслуживание</p>
-				<div class="text">
-					По вашему желанию мы демонтируем старое покрытие, выполним ремонт
-					механических повреждений покрытия «жидкая резина» и многое другое.
-				</div>
-			</li>
-		</ul>
-	</div>
-</section>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"contacts", 
+	"bitrix:main.include",
+	"",
+	Array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "your_benefits",
+		"EDIT_TEMPLATE" => ""
+	)
+);?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"contacts",
 	array(
 		"COMPONENT_TEMPLATE" => "contacts",
 		"IBLOCK_TYPE" => "content",
